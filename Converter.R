a78a335daf1d03bbec8af39531b90fab91dc15e4
Glauco1990 <glaucoestatistico@gmail.com>
2020-08-20 19:34:26 -0300

    Evolui com o código para ler varios arquivos da pasta

--- Converter.R
+++ Converter.R
@@ -1,6 +1,45 @@
-
+# ler um arquivo
 arquivo <- list.files("DATASUS", pattern = ".dbc")
 caminho <- stringr::str_c("DATASUS/",arquivo)
+saida <- stringr::str_c("CONVERSÕES/PAHUAB",stringr::str_sub(arquivo, start = 5, end = 8))
+a <- read.dbc::read.dbc(caminho)
+a <- dplyr::filter(a, PA_CODUNI == "4014111")
+readr::write_rds(a, path = saida)
+
+# ler varios arquivos
+arquivo <- list.files("DATASUS", pattern = ".dbc")
+
+caminho <- purrr::map(arquivo[1], function(a) {stringr::str_c("DATASUS/",arquivo)})
+
+saida <- purrr::map(arquivo[1], function(a) {stringr::str_c("CONVERSÕES/PAHUAB",stringr::str_sub(arquivo, start = 5, end = 8))})
+
+a <- purrr::map(caminho[1], read.dbc::read.dbc)
+a <- dplyr::filter(a, PA_CODUNI == "4014111")
+readr::write_rds(a, path = saida)
+
+# Com for
+arquivo <- list.files("DATASUS", pattern = ".dbc")
+caminho <- NULL
+saida <- NULL
+a <- NULL
+b <- NULL
+for (i in 1 : length(arquivo))
+{
+caminho[i] <- stringr::str_c("DATASUS/",arquivo[i])
+saida[i] <- stringr::str_c("CONVERSÕES/PAHUAB",stringr::str_sub(arquivo[i], start = 5, end = 8),".rds")
+}
+
+dados <- purrr::map(caminho, read.dbc::read.dbc)
+
+for (i in 1 : length(arquivo))
+{
+b[[i]] <- dplyr::filter(dados[[i]], PA_CODUNI == "4014111")
+}
+
+
+
+
+
 saida <- stringr::str_c("CONVERSÕES/PAHUAB",stringr::str_sub(arquivo, start = 5, end = 8))
 a <- read.dbc::read.dbc(caminho)
 a <- dplyr::filter(a, PA_CODUNI == "4014111")
